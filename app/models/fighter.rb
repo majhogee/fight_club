3228730dc66ec261ca72da9f64bc2231a7db8b10
majhogee <jo.wojtowicz@gmail.com>
2016-02-25 17:37:28 +0100

    accepts_nested_attributes_

--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -1,4 +1,5 @@
 class Fighter < ActiveRecord::Base
 	has_many :skills
+	accepts_nested_attributes_for :skills, reject_if: :all_blank, allow_destroy: true
 	mount_uploader :picture, PictureUploader
 end
